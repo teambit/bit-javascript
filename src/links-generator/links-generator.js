fecac313299ca41641406f49553c84493f5e2c4d
David First <david@cocycles.com>
2017-04-25 12:45:19 -0400

    refactor: remove duplications in links-generator

--- src/links-generator/links-generator.js
+++ src/links-generator/links-generator.js
@@ -81,6 +81,13 @@ export function dependencies(
   });
 }
 
+function generateLinkP(targetModuleDir, namespace, name, map, id) {
+  const targetDir = path.join(targetModuleDir, namespace, name, INDEX_JS);
+  const relativeComponentsDir = path.join(...Array(4).fill('..'), INLINE_COMPONENTS_DIRNAME);
+  const dependencyDir = path.join(relativeComponentsDir, map[id].loc, map[id].file);
+  return writeFileP(targetDir, linkTemplate(dependencyDir));
+}
+
 export function publicApiForInlineComponents(targetModuleDir: string, inlineMap: Object):
 Promise<Object> {
   const components = {};
@@ -89,10 +96,7 @@ Promise<Object> {
   const writeAllFiles = Promise.all(Object.keys(inlineMap).map((id) => {
     const [namespace, name] = id.split(path.sep);
     components[`${namespace}/${name}`] = id;
-    const targetDir = path.join(targetModuleDir, namespace, name, INDEX_JS);
-    const relativeComponentsDir = path.join(...Array(4).fill('..'), INLINE_COMPONENTS_DIRNAME);
-    const dependencyDir = path.join(relativeComponentsDir, inlineMap[id].loc, inlineMap[id].file);
-    return writeFileP(targetDir, linkTemplate(dependencyDir));
+    return generateLinkP(targetModuleDir, namespace, name, inlineMap, id);
   }));
 
   return writeAllFiles.then(() => components);
@@ -137,10 +141,7 @@ Promise<Object> {
   const writeAllFiles = exportPendingComponents.map((component) => {
     const [namespace, name] = map[component].loc.split(ID_DELIMITER);
     components[`${namespace}/${name}`] = component;
-    const targetDir = path.join(targetModuleDir, namespace, name, INDEX_JS);
-    const relativeComponentsDir = path.join(...Array(4).fill('..'), COMPONENTS_DIRNAME);
-    const dependencyDir = path.join(relativeComponentsDir, map[component].loc, map[component].file);
-    return writeFileP(targetDir, linkTemplate(dependencyDir));
+    return generateLinkP(targetModuleDir, namespace, name, map, component);
   });
   return Promise.all(writeAllFiles).then(() => ({ map, components }));
 }
@@ -153,13 +154,10 @@ export function publicApiComponentLevel(targetModuleDir: string, map: Object,
   }
   const writeAllFiles = Object.keys(projectBitJson.dependencies).map((id) => {
     const [, namespace, name] = id.split(ID_DELIMITER);
-    const targetDir = path.join(targetModuleDir, namespace, name, INDEX_JS);
     const mapId = id + VERSION_DELIMITER + projectBitJson.dependencies[id];
     if (!map[mapId]) return Promise.resolve(); // the file is in bit.json but not fetched yet
     components[`${namespace}/${name}`] = mapId;
-    const relativeComponentsDir = path.join(...Array(4).fill('..'), COMPONENTS_DIRNAME);
-    const dependencyDir = path.join(relativeComponentsDir, map[mapId].loc, map[mapId].file);
-    return writeFileP(targetDir, linkTemplate(dependencyDir));
+    return generateLinkP(targetModuleDir, namespace, name, map, mapId);
   });
   return Promise.all(writeAllFiles).then(() => components);
 }
