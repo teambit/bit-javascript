3326ca901d41ad22c44244eebc6ae3dc8923dfda
David First <david@cocycles.com>
2017-04-25 16:42:42 -0400

    print fetch-command results

--- src/cli/commands/fetch-cmd.js
+++ src/cli/commands/fetch-cmd.js
@@ -1,7 +1,13 @@
 // @flow
-
+import chalk from 'chalk';
 import { type Command } from './types';
 import { fetchAction } from '../../actions';
+import { VERSION_DELIMITER } from '../../constants';
+
+const printComponents = components => components.map((component) => {
+  const [name, version] = component.split(VERSION_DELIMITER);
+  return `\t> ${name} - ${version || 'latest'}`;
+}).join('\n');
 
 const fetchCommand: Command = {
   name: 'fetch',
@@ -13,7 +19,9 @@ const fetchCommand: Command = {
     },
   ],
   action: args => fetchAction((args && args.ids) || []),
-  report: () => 'done',
+  report: components => chalk.underline('successfully fetched the following Bit components.\n')
+  + chalk.cyan(printComponents(components)),
+  loaderText: 'Fetching components',
   loader: true,
 };
 
