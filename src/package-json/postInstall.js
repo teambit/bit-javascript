322bf3a08930f63a8fb9a63a6a6d5b3182bb1fb7
Amit <amit@cocycles.com>
2017-10-26 15:53:44 +0300

    support creating script for links

--- src/package-json/postInstall.js
+++ src/package-json/postInstall.js
@@ -1,8 +1,27 @@
 const fs = require('fs');
 const path = require('path');
-
 const postInstallScriptName = 'bitBindings.js';
 
+type LinkProps = {
+  packagePath: string,
+  indexName: string,
+  fileContent: string
+};
+class LinkData {
+  packagePath: string;
+  indexName: string;
+  fileContent: string;
+
+  constructor( { packagePath, indexName, fileContent }: LinkProps) {
+    this.packagePath = packagePath;
+    this.indexName = indexName;
+    this.fileContent = fileContent;
+  }
+
+  toString() {
+    return JSON.stringify(this);
+  }
+}
 const scriptTemplate = (data) => `const fs = require('fs');
 const path = require('path');
 const arr = ${data}
@@ -13,15 +32,14 @@ function mkdirp(filepath) {
     }
     fs.mkdirSync(filepath);
 } 
-arr.forEach(stringObj => {
-    const link = JSON.parse(stringObj)    
-    mkdirp(path.join(__dirname,'node_modules', link.packagePath));
-    const filePath = path.join(__dirname,'node_modules', link.packagePath, link.indexName);
-        fs.writeFileSync(filePath, link.fileContent)
+arr.forEach(linkMetaData => { 
+    mkdirp(path.join(__dirname,'node_modules', linkMetaData.packagePath));
+    const filePath = path.join(__dirname,'node_modules', linkMetaData.packagePath, linkMetaData.indexName);
+        fs.writeFileSync(filePath, linkMetaData.fileContent)
 })`;
 
-export default  function generatePostInstallScript (writeDir: string, linkArrays: Array<Object>) {
-  const linkArray = linkArrays.map(linkObj => JSON.stringify(linkObj))
-  fs.writeFileSync(path.join(writeDir, postInstallScriptName), scriptTemplate(JSON.stringify(linkArray)));
-  return { postinstall : `node ${postInstallScriptName}` } ;
+export default  function generatePostInstallScript (writeDir: string, linkMeta: Array<Object>) {
+  const LinkArr = linkMeta.map(linkObj => new LinkData(linkObj));
+  fs.writeFileSync(path.join(writeDir, postInstallScriptName), scriptTemplate(JSON.stringify(LinkArr)));
+  return { postinstall : `node ${postInstallScriptName}` };
 }
