1aaa05fe7e7bc2ab54124d1c97e98e9478eda464
Amit <amit@cocycles.com>
2017-10-26 16:13:14 +0300

    added check if file exists

--- src/pack/pack.js
+++ src/pack/pack.js
@@ -4,13 +4,16 @@ import childProcessP from 'child-process-promise';
 
 const exec = childProcessP.exec
 
-export default async function pack(cwd: string, outputPath: string): string {
+export default async function pack(cwd: string, outputPath: string, force: boolean = false): string {
   const pjson = require(path.join(cwd,'package.json'));
   const tgzName = `${pjson.name}-${pjson.version}.tgz`;
   const tgzOriginPath = path.join(cwd, tgzName)
   const tgzDestinationPath = path.join(outputPath, tgzName);
 
   await exec('npm pack', { cwd })
+  if (fs.pathExistsSync(tgzDestinationPath) && force){
+    fs.removeSync(tgzDestinationPath);
+  }
   await fs.move(tgzOriginPath, tgzDestinationPath)
   return tgzDestinationPath;
 }
