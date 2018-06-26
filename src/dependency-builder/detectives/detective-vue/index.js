b987f936f87acca292b58bde78667a845da7a8e3
David First <david@cocycles.com>
2018-06-26 13:10:47 +0300

    fix Vue caching issue between files

--- src/dependency-builder/detectives/detective-vue/index.js
+++ src/dependency-builder/detectives/detective-vue/index.js
@@ -1,22 +1,21 @@
-const compiler = require('vue-template-compiler');
-
-const finalDependencies = {};
-const addDependencies = (dependencies, isScript) => {
-  let objDependencies = {};
-  if (Array.isArray(dependencies)) {
-    dependencies.forEach((dependency) => {
-      objDependencies[dependency] = {};
-    });
-  } else {
-    objDependencies = dependencies;
-  }
-  Object.keys(objDependencies).forEach((dependency) => {
-    finalDependencies[dependency] = objDependencies[dependency];
-    finalDependencies[dependency].isScript = isScript;
-  });
-};
-
 module.exports = function (src, options = {}) {
+  const compiler = require('vue-template-compiler');
+  const finalDependencies = {};
+  const addDependencies = (dependencies, isScript) => {
+    let objDependencies = {};
+    if (Array.isArray(dependencies)) {
+      dependencies.forEach((dependency) => {
+        objDependencies[dependency] = {};
+      });
+    } else {
+      objDependencies = dependencies;
+    }
+    Object.keys(objDependencies).forEach((dependency) => {
+      finalDependencies[dependency] = objDependencies[dependency];
+      finalDependencies[dependency].isScript = isScript;
+    });
+  };
+
   options.useContent = true;
   const { script, styles } = compiler.parseComponent(src, { pad: 'line' });
   // it must be required here, otherwise, it'll be a cyclic dependency
@@ -26,8 +25,10 @@ module.exports = function (src, options = {}) {
     addDependencies(dependencies, true);
   }
   if (styles) {
-    const dependencies = precinct(style.content, { type: style.lang || 'scss' });
-    styles.map(style => addDependencies(dependencies, false));
+    styles.forEach((style) => {
+      const dependencies = precinct(style.content, { type: style.lang || 'scss' });
+      addDependencies(dependencies, false);
+    });
   }
 
   return finalDependencies;
