1213b7d80dbf2ca7c30733413d7750a9644c6e1d
David First <david@bitsrc.io>
2019-10-18 11:28:14 -0400

    fix lint

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,6 +1,6 @@
 // import bindAction, { bindSpecificComponentsAction } from './bind';
 import getDependenciesAction from './get-dependencies';
-
+// eslint-disable-next-line import/prefer-default-export
 export {
   // bindAction,
   // bindSpecificComponentsAction,
